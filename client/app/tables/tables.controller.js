d5b849752318009cd54e09e4347ceb7db459863c
Matt Faulkner <mnfaulk@gmail.com>
2014-11-13 18:18:38 -0800

    minor edit

--- client/app/tables/tables.controller.js
+++ client/app/tables/tables.controller.js
@@ -3,10 +3,10 @@
 angular.module('csvExplorerClientApp')
   .controller('TablesCtrl', function ($scope) {
   	$scope.tableName = "table_name.csv";
-    $scope.myData = [{name: "Moroni", age: 50},
-                     {name: "Tiancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Enos", age: 34}];
+    $scope.myData = [{name: "A", age: 50},
+                     {name: "B", age: 43},
+                     {name: "C", age: 27},
+                     {name: "D", age: 29},
+                     {name: "E", age: 34}];
     $scope.gridOptions = { data: 'myData' };
   });
